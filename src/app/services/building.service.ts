1b3525a37182e15bb0b9911c34d4c6c3c0226fc1
Terrance Mount <thorelore@gmail.com>
2018-08-26 08:07:07 -0800

    Service skeleton gen

--- src/app/services/building.service.ts
+++ src/app/services/building.service.ts
@@ -6,4 +6,20 @@ import { Injectable } from '@angular/core';
 export class BuildingService {
 
   constructor() { }
+
+
+  getBuildings(){
+    return DEFAULT_BUILDINGS;
+  }
 }
+const DEFAULT_BUILDINGS = [{
+  id: 1,
+  fullName: "Engineering and Industry Building",
+  shortName: "EIB",
+  iconPath:'../assets/img/EIB-small.jpg'
+},{
+  id: 2,
+  fullName: 'Eugene Short Hall',
+  shortName: "ESH",
+  iconPath: "../asset/img/EIB-small.jpg"
+}]
--- src/app/services/slideshow.service.ts
+++ src/app/services/slideshow.service.ts
@@ -6,4 +6,26 @@ import { Injectable } from '@angular/core';
 export class SlideshowService {
 
   constructor() { }
+
+  //get json data of slides with urls and times to do it
+  getSlides(){
+    //return an array of slide models
+  }
+
+  //get json data on a particular slide
+  getSlide(slideId:number){
+    //return slide model
+  }
+
+  postSlide(slideModel){
+    //return the id of the new slide
+  }
+
+  updateSlide(slideModel){
+    //return the id of the updated slide
+  }
+
+  deleteSlide(){
+    //return boolean if it was deleted
+  } 
 }
